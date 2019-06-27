d5c9c885fe411cf45a90ea2e06b996ca1cdc5d51
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-06-27 20:27:20 -0300

    Added solution for PROBLEM: 1005 - Average 1

---
+++ URI Online Judge/Beginner/average_1.py
@@ -0,0 +1,16 @@
+A = round(float(input()),1)
+B = round(float(input()),1)
+
+MEDIA = (A*3.5 + B*7.5)/11
+
+print("MEDIA = %.5f" % MEDIA)
+
+
+
+A = round(float(input()),1)
+B = round(float(input()),1)
+C = round(float(input()),1)
+
+MEDIA = (A*2 + B*3 + C*5)/10
+
+print("MEDIA = %.1f" % MEDIA)
