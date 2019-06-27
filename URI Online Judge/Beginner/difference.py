fe3f38bcc84c8a30e252768847434d6cc830198b
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-06-27 20:42:47 -0300

    Added solution for PROBLEM: 1007 - Difference

---
+++ URI Online Judge/Beginner/difference.py
@@ -0,0 +1,15 @@
+# URI ONLINE JUDGE ##############################################
+# PROBLEM: 1007 - Difference
+# LANGUAGE: Python 3 (Python 3.4.3)
+# DEVELOPER: Marcus Marinho
+# GITHUB: https://github.com/marcusmarinhob
+#################################################################
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+DIFF = A * B - C * D
+
+print("DIFERENCA =", DIFF)
