6b6695943b72940e94820f88d3b48b020bb3bbf9
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-06-26 22:27:00 -0300

    Added solution for PROBLEM: 1003 - Simple Sum.

---
+++ URI Online Judge/Beginner/simple_sum.py
@@ -0,0 +1,13 @@
+# URI ONLINE JUDGE ##############################################
+# PROBLEM: 1003 - Simple Sum
+# LANGUAGE: Python 3 (Python 3.4.3)
+# DEVELOPER: Marcus Marinho
+# GITHUB: https://github.com/marcusmarinhob
+#################################################################
+
+A = int(input())
+B = int(input())
+
+SOMA = A + B
+
+print("SOMA =", SOMA)
