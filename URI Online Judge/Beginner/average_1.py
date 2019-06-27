03f1a81d8be1cc87cabea4b6404f7e7dec2e73d8
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-06-27 20:31:34 -0300

    Added header of average_1.py

--- URI Online Judge/Beginner/average_1.py
+++ URI Online Judge/Beginner/average_1.py
@@ -1,3 +1,10 @@
+# URI ONLINE JUDGE ##############################################
+# PROBLEM: 1005 - Average 1
+# LANGUAGE: Python 3 (Python 3.4.3)
+# DEVELOPER: Marcus Marinho
+# GITHUB: https://github.com/marcusmarinhob
+#################################################################
+
 A = round(float(input()),1)
 B = round(float(input()),1)
 
