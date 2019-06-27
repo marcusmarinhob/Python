05880769bc8508b2fbccd12d52d15956422cb150
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-06-26 22:16:37 -0300

    Added solution for PROBLEM: 1002 - Area of a Circle.

---
+++ URI Online Judge/Beginner/area_of_a_circle.py
@@ -0,0 +1,13 @@
+# URI ONLINE JUDGE ##############################################
+# PROBLEM: 1002 - Area of a Circle
+# LANGUAGE: Python 3 (Python 3.4.3)
+# DEVELOPER: Marcus Marinho
+# GITHUB: https://github.com/marcusmarinhob
+#################################################################
+
+R = float(input())
+pi = 3.14159
+
+A = pi*(R**2)
+
+print("A=%.4f" %A)
