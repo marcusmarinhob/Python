0968933ac685f72302dc96f200369fc164f67a98
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-06-26 22:22:22 -0300

    Added solution for PROBLEM: 1001 - Extremely Basic.

---
+++ URI Online Judge/Beginner/extremely_basic.py
@@ -0,0 +1,11 @@
+# URI ONLINE JUDGE ##############################################
+# PROBLEM: 1001 - Extremely Basic
+# LANGUAGE: Python 3 (Python 3.4.3)
+# DEVELOPER: Marcus Marinho
+# GITHUB: https://github.com/marcusmarinhob
+#################################################################
+
+A = int(input())
+B = int(input())
+
+print("X = %d" %(A+B))
