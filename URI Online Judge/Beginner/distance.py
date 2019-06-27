6ac9604223f45e7f0a319dc1c8563aeac52eea4c
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-06-27 20:52:37 -0300

    Added solution for PROBLEM: 1016 - Distance

---
+++ URI Online Judge/Beginner/distance.py
@@ -0,0 +1,17 @@
+# URI ONLINE JUDGE ##############################################
+# PROBLEM: 1016 - Distance
+# LANGUAGE: Python 3 (Python 3.4.3)
+# DEVELOPER: Marcus Marinho
+# GITHUB: https://github.com/marcusmarinhob
+#################################################################
+
+v1 = 60.0
+v2 = 90.0
+
+dist = int(input())
+
+time = 60*dist/(v2-v1)
+
+
+print("%d minutos" % time)
+
