56ade9c4b442969c6114152e046125489ddefcec
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-06-28 14:09:27 -0300

    Updated file TCPClient.py

--- Computer Networks/TCP Client-Server/TCPClient.py
+++ Computer Networks/TCP Client-Server/TCPClient.py
@@ -4,3 +4,13 @@
 # GITHUB: https://github.com/marcusmarinhob
 #################################################################
 
+from socket import *
+serverName = 'servername'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+sentence = raw_input('Input lowercase sentence:')
+clientSocket.send(sentence)
+modifiedSentence = clientSocket.recv(1024)
+print('From Server:', modifiedSentence)
+clientSocket.close()
