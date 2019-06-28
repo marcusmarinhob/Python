49a80a071c5ece8dc0b542d5c299da82872f2727
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-06-28 14:20:26 -0300

    Updated file TCPServer.py

--- Computer Networks/TCP Client-Server/TCPServer.py
+++ Computer Networks/TCP Client-Server/TCPServer.py
@@ -8,7 +8,7 @@
 from socket import *
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind((’’,serverPort))
+serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 print("The server is ready to receive")
 
