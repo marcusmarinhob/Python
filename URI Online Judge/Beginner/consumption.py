549c0623701662aec5360fe809a805f3d6755d58
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-06-30 10:12:44 -0300

    Added solution for PROBLEM: 1014 - Consumption

---
+++ URI Online Judge/Beginner/consumption.py
@@ -0,0 +1,14 @@
+# URI ONLINE JUDGE ##############################################
+# PROBLEM: 1014 - Consumption
+# LANGUAGE: Python 3 (Python 3.4.3)
+# DEVELOPER: Marcus Marinho
+# GITHUB: https://github.com/marcusmarinhob
+#################################################################
+
+X = int(input()) #Distance Km
+Y = float(input()) #Spent fuel total
+
+consumption = X/Y
+
+
+print("%.3f km/l" % consumption)
