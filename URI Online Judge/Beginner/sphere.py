6be0232457ced06b4c40c578c8f8f661929d3310
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-06-30 09:56:29 -0300

    Added solution for PROBLEM: 1011 - Sphere

---
+++ URI Online Judge/Beginner/sphere.py
@@ -0,0 +1,14 @@
+# URI ONLINE JUDGE ##############################################
+# PROBLEM: 1011 - Sphere
+# LANGUAGE: Python 3 (Python 3.4.3)
+# DEVELOPER: Marcus Marinho
+# GITHUB: https://github.com/marcusmarinhob
+#################################################################
+
+
+R = int(input())
+pi = 3.14159
+
+vol = (4.0/3.0)*pi*R**3 
+
+print("VOLUME = %.3f" % vol)
