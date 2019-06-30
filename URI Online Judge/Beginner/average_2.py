feda635deab1c9a9f1dad375984d6a10c707d2e0
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-06-30 09:49:15 -0300

    Added solution for PROBLEM: 1006 - Average 2 and updated average_1.py

--- URI Online Judge/Beginner/average_2.py
+++ URI Online Judge/Beginner/average_2.py
@@ -1,14 +1,16 @@
 # URI ONLINE JUDGE ##############################################
-# PROBLEM: 1005 - Average 1
+# PROBLEM: 1006 - Average 2
 # LANGUAGE: Python 3 (Python 3.4.3)
 # DEVELOPER: Marcus Marinho
 # GITHUB: https://github.com/marcusmarinhob
 #################################################################
 
+
+
 A = round(float(input()),1)
 B = round(float(input()),1)
+C = round(float(input()),1)
 
-MEDIA = (A*3.5 + B*7.5)/11
+MEDIA = (A*2 + B*3 + C*5)/10
 
-print("MEDIA = %.5f" % MEDIA)
-
+print("MEDIA = %.1f" % MEDIA)
