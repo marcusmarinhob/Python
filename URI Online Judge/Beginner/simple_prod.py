ac17a18d9b9ba73a680de24bc0c9f4e8a2a91fc1
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-07-03 20:53:36 -0300

    Updated header of simple_prod.py

--- URI Online Judge/Beginner/simple_prod.py
+++ URI Online Judge/Beginner/simple_prod.py
@@ -1,3 +1,10 @@
+# URI ONLINE JUDGE ##############################################
+# PROBLEM: 1004 - Simple Product
+# LANGUAGE: Python 3 (Python 3.4.3)
+# DEVELOPER: Marcus Marinho
+# GITHUB: https://github.com/marcusmarinhob
+#################################################################
+
 A = int(input())
 B = int(input())
 
