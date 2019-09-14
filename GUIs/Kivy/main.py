a7e672a58045c424b6c24207eb1cd0049c1a709f
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-09-13 21:49:47 -0300

    1. Adding new comments for in the code

--- GUIs/Kivy/main.py
+++ GUIs/Kivy/main.py
@@ -1,8 +1,13 @@
-from kivy.app import App                   # Importing class kivy.app in order to create a new app
-from kivy.uix.button import Button         # Importing class kivy.uix.button in order to create a new button object 
+## @package main.py
+# This module creates a GUI based on Kivy Framework.
+# 
+# This first application imports the class kivy.app and kivy.uix.button both from Kivy Framework.
+
+from kivy.app import App                        # Importing class kivy.app in order to create a new app
+from kivy.uix.button import Button              # Importing class kivy.uix.button in order to create a new button object 
 
 class Test(App):                                # Creating the class Test inheriting class App properties
-    def build(self):                                    # New method for the class Test
-        return Button(text = 'Hello World!')            # Creation of the Button ('Hello World')
+    def build(self):                            # New method for the class Test
+        return Button(text = 'Hello World!')    # Creation of the Button ('Hello World')
 
-Test().run()                               # Run the Test app
+Test().run()                                    # Run the Test app
