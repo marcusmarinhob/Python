f41ec0d6708a112d64d40efcd4e9d6da8505d3be
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-09-19 21:57:17 -0300

    1. Added Auto git saving to the code

--- Coursera/01-Py4e/02-SequentialProgram/Repeated.py
+++ Coursera/01-Py4e/02-SequentialProgram/Repeated.py
@@ -4,4 +4,16 @@ while n > 0:
     print(n)
     n = n -1
 
-print("Blastoff!")
+print('Blastoff!')
+
+
+
+# Auto git saving #####################################################
+import os
+
+fileName = os.path.basename(__file__)
+commitMsg = "1. Added Auto git saving to the code" 
+
+os.system("git add "+ fileName)
+print('git commit -m '+'"'+commitMsg+'"')
+os.system('git commit -m '+'"'+commitMsg+'"')
