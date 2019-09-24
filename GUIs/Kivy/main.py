8bbab57c2b224bcc380916634f99f700f730f070
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-09-23 21:40:50 -0300

    1. Added functionalities of increment and decremente the label text

--- GUIs/Kivy/main.py
+++ GUIs/Kivy/main.py
@@ -12,14 +12,22 @@ class Test(App):                                    # Creating the class Test in
     def build(self):                                # New method for the class Test
         box = BoxLayout(orientation='vertical')     # Creation of a box layout
         
-        button1 = Button(text='Button1')            # Creation of the Button1
-        button2 = Button(text='Button2')            # Creation of the Button2
-        label1 = Label(text='Label1')               # Creation of the Label1
+        self.button1 = Button(text='Increment', on_release=self.increment)      # Creation of the Button1
+        self.button2 = Button(text='Decrement', on_release=self.decrement)      # Creation of the Button2
+        self.label1 = Label(text='0', font_size='50')                           # Creation of the Label1
         
-        box.add_widget(button1)                     # Adding button1 the box 
-        box.add_widget(button2)                     # Adding button2 the box 
-        box.add_widget(label1)                      # Adding button2 the box 
+        box.add_widget(self.button1)                     # Adding button1 the box 
+        box.add_widget(self.button2)                     # Adding button2 the box 
+        box.add_widget(self.label1)                      # Adding button2 the box 
 
         return box    
 
-Test().run()                                        # Run the Test app
+    def increment(self, button):
+        self.label1.text = str(int(self.label1.text)+1)
+
+    def decrement(self, button):
+        self.label1.text = str(int(self.label1.text)-1)
+
+
+
+Test().run()    # Run the Test app
