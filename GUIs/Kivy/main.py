ef47d0c9ba560f8c382a981a68949e7ce2b9c503
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-09-23 21:53:41 -0300

    1. Added Kivy file to the project and imported a button and one label on it.

--- GUIs/Kivy/main.py
+++ GUIs/Kivy/main.py
@@ -4,30 +4,13 @@
 # This application imports widgets from Kivy Framework.
 
 from kivy.app import App                            # Importing class kivy.app in order to create a new app
-from kivy.uix.button import Button                  # Importing class kivy.uix.button in order to create a new button object 
 from kivy.uix.boxlayout import BoxLayout            # Importing class kivy.uix.boxlayout in order to create a new layout scheme
-from kivy.uix.label import Label                    # Importing class kivy.uix.label in order to create a new label object 
+
+class Incrementer(BoxLayout):
+    pass
 
 class Test(App):                                    # Creating the class Test inheriting class App properties
     def build(self):                                # New method for the class Test
-        box = BoxLayout(orientation='vertical')     # Creation of a box layout
-        
-        self.button1 = Button(text='Increment', on_release=self.increment)      # Creation of the Button1
-        self.button2 = Button(text='Decrement', on_release=self.decrement)      # Creation of the Button2
-        self.label1 = Label(text='0', font_size='50')                           # Creation of the Label1
-        
-        box.add_widget(self.button1)                     # Adding button1 the box 
-        box.add_widget(self.button2)                     # Adding button2 the box 
-        box.add_widget(self.label1)                      # Adding button2 the box 
-
-        return box    
-
-    def increment(self, button):
-        self.label1.text = str(int(self.label1.text)+1)
-
-    def decrement(self, button):
-        self.label1.text = str(int(self.label1.text)-1)
-
-
+        return Incrementer()    
 
 Test().run()    # Run the Test app
