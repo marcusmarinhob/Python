e980f8098812e685c4be897200f1b72f07fcfb02
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-10-07 21:35:18 -0300

    1. Seconds conversion.

--- Coursera/Python3Programming/01-PythonBasics/Input.py
+++ Coursera/Python3Programming/01-PythonBasics/Input.py
@@ -3,13 +3,20 @@ os.system('cls')
 
 print('\n# BEGIN OF THE SCRIPT #############################################')
 
-name = input("Please enter your name: ")
-print("Hello,", name)
+str_seconds = input("Please enter the number of seconds you wish to convert: ")
+total_secs = int(str_seconds)
+
+hours = total_secs // 3600
+secs_still_remaining = total_secs % 3600
+minutes = secs_still_remaining // 60
+secs_finally_remaining = secs_still_remaining % 60
+
+print("Hrs= ", hours, "mins=", minutes, "secs=", secs_finally_remaining)
 
 
 # Auto git saving #####################################################
 save = 1
-commitMsg = '1. Input your name.' 
+commitMsg = '1. Seconds conversion.' 
 
 if save:
     print('\nAUTO GIT SAVING...')
