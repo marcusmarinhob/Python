9326d832640016bf419930ff674e91d5a22b8ff2
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-10-19 18:42:43 -0300

    1. Loop for.

---
+++ Coursera/Python3Programming/01-PythonBasics/LoopFor.py
@@ -0,0 +1,32 @@
+import os
+os.system('cls')
+
+print('\n# BEGIN OF THE SCRIPT #############################################')
+
+inviteList = ["Joe", "Amy", "Brad", "Angelina", "Zuki", "Thandi", "Paris", "Marcus"]
+
+for name in inviteList:
+    print("Hi", name, "Please come to my party on Saturday!")
+
+
+
+# Auto git saving #####################################################
+save = 1
+commitMsg = '1. Loop for.' 
+
+if save:
+    print('\nAUTO GIT SAVING...')
+    
+    fileName = os.path.basename(__file__)    
+
+    os.system('git add '+ fileName)
+    os.system('git commit -m '+'"'+commitMsg+'"')
+    os.system('git push')
+
+    print('\nFile Name: '+fileName)
+    print('\nCommit Message: '+commitMsg)
+    print('\nSAVED AND PUSHED!')
+
+
+print('\n# END OF THE SCRIPT ###############################################')
+#print('\n'*15)
