e6b89f9d4439453b039bd590c23369770424383b
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-10-20 09:34:55 -0300

    1. Aliasing

---
+++ Coursera/Python3Programming/01-PythonBasics/Aliasing.py
@@ -0,0 +1,32 @@
+import os
+os.system('cls')
+
+print('\n# BEGIN OF THE SCRIPT #############################################')
+
+a = [81, 82, 83]
+b = a
+print(a is b)
+
+
+
+
+# Auto git saving #####################################################
+save = 1
+commitMsg = '1. Aliasing' 
+
+if save:
+    print('\nAUTO GIT SAVING...')
+    
+    fileName = os.path.basename(__file__)    
+
+    os.system('git add '+ fileName)
+    os.system('git commit -m '+'"'+commitMsg+'"')
+    os.system('git push')
+
+    print('\nFile Name: '+fileName)
+    print('\nCommit Message: '+commitMsg)
+    print('\nSAVED AND PUSHED!')
+
+
+print('\n# END OF THE SCRIPT ###############################################')
+#print('\n'*15)
