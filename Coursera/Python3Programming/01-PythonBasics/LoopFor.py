a4ec4436e12d702ddf6e560eb0eec4b4d9e8eaeb
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-10-19 21:47:44 -0300

    1. Loop for - Enumerate.

--- Coursera/Python3Programming/01-PythonBasics/LoopFor.py
+++ Coursera/Python3Programming/01-PythonBasics/LoopFor.py
@@ -3,27 +3,12 @@ os.system('cls')
 
 print('\n# BEGIN OF THE SCRIPT #############################################')
 
-print("range(5): ")
-for i in range(5):
-    print(i)
-
-print("range(0,5): ")
-for i in range(0, 5):
-    print(i)
-
-# Notice the casting of `range` to the `list`
-print(list(range(5)))
-print(list(range(0,5)))
-
-# Note: `range` function is already casted as `list` in the textbook
-print(range(5))
-
-
-
+for counter, item in enumerate(['apple', 'pear', 'apricot', 'cherry', 'peach']):
+    print(counter, item)
 
 # Auto git saving #####################################################
 save = 1
-commitMsg = '1. Loop for and Turtle - Range.' 
+commitMsg = '1. Loop for - Enumerate.' 
 
 if save:
     print('\nAUTO GIT SAVING...')
