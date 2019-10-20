8c1defa69d60ce7fc29806ce10f8b51273a00b81
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-10-20 09:13:07 -0300

    1. Multability.

--- Coursera/Python3Programming/01-PythonBasics/Multability.py
+++ Coursera/Python3Programming/01-PythonBasics/Multability.py
@@ -3,10 +3,11 @@ os.system('cls')
 
 print('\n# BEGIN OF THE SCRIPT #############################################')
 
-alist = ['a', 'b', 'c', 'd', 'e', 'f']
-alist[1:3] = []
+alist = ['a', 'd', 'f']
+alist[1:1] = ['b', 'c']
 print(alist)
-
+alist[4:4] = ['e']
+print(alist)
 
 
 
