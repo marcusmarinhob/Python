65147842c2aa142000db6b9b34f110d4c67b93b3
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-10-21 20:15:43 -0300

    1. Non-mutating Methods on Strings

--- Coursera/Python3Programming/01-PythonBasics/NonMutatingMethodsOnStrings.py
+++ Coursera/Python3Programming/01-PythonBasics/NonMutatingMethodsOnStrings.py
@@ -3,15 +3,11 @@ os.system('cls')
 
 print('\n# BEGIN OF THE SCRIPT #############################################')
 
-ss = "    Hello, World    "
-
-els = ss.count("l")
-print(els)
-
-print("***"+ss.strip()+"***")
-
-news = ss.replace("o", "***")
-print(news)
+scores = [("Rodney Dangerfield", -1), ("Marlon Brando", 1), ("You", 100)]
+for person in scores:
+    name = person[0]
+    score = person[1]
+    print("Hello " + name + ". Your score is " + str(score))
 
 
 
