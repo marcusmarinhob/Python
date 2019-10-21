94528356220a4fe8f1251a8c18bbec498a940949
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-10-21 20:19:31 -0300

    1. Non-mutating Methods on Strings

--- Coursera/Python3Programming/01-PythonBasics/NonMutatingMethodsOnStrings.py
+++ Coursera/Python3Programming/01-PythonBasics/NonMutatingMethodsOnStrings.py
@@ -7,7 +7,7 @@ scores = [("Rodney Dangerfield", -1), ("Marlon Brando", 1), ("You", 100)]
 for person in scores:
     name = person[0]
     score = person[1]
-    print("Hello " + name + ". Your score is " + str(score))
+    print("Hello {}. Your score is {}.".format(name, score))
 
 
 
