e9222833345fa44270ff1f264d609e084fd291c3
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-10-23 19:54:04 -0300

    1. Writing text files

---
+++ Coursera/Python3Programming/02-PythonFunctionsFilesAndDictionaries/WritingFile.py
@@ -0,0 +1,40 @@
+import os
+os.system('cls')
+
+print('\n# BEGIN OF THE SCRIPT #############################################')
+
+filename = "squared_numbers.txt"
+outfile = open(filename, "w")
+
+for number in range(1, 13):
+    square = number * number
+    outfile.write(str(square) + "\n")
+
+outfile.close()
+
+infile = open(filename, "r")
+print(infile.read()[:10])
+
+
+
+
+# Auto git saving #####################################################
+save = 1
+commitMsg = '1. Writing text files' 
+
+if save:
+    print('\nAUTO GIT SAVING...')
+    
+    fileName = os.path.basename(__file__)    
+
+    os.system('git add '+ fileName)
+    os.system('git commit -m '+'"'+commitMsg+'"')
+    os.system('git push')
+
+    print('\nFile Name: '+fileName)
+    print('\nCommit Message: '+commitMsg)
+    print('\nSAVED AND PUSHED!')
+
+
+print('\n# END OF THE SCRIPT ###############################################')
+#print('\n'*15)
