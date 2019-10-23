7a997ac919dfec37c17dae83408e8c445722e3b0
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-10-23 20:19:05 -0300

    1. Writing data to a CSV File

--- Coursera/Python3Programming/02-PythonFunctionsFilesAndDictionaries/WritingFile.py
+++ Coursera/Python3Programming/02-PythonFunctionsFilesAndDictionaries/WritingFile.py
@@ -3,15 +3,27 @@ os.system('cls')
 
 print('\n# BEGIN OF THE SCRIPT #############################################')
 
-with open('mydata.txt', 'r') as md:
-    for line in md:
-        print(line)
+olympians = [("John Aalberg", 31, "Cross Country Skiing"),
+            ("Minna Maarit Aalto", 30, "Sailing"),
+            ("Win Valdemar Aaltonen", 54, "Art Competitions"),
+            ("Wakako Abe", 18, "Cycling")]
+
+outfile = open("reduced_olympics.csv","w")
+# output the header row
+outfile.write('Name,Age,Sport')
+outfile.write('\n')
+# output each of the rows:
+for olympian in olympians:
+    row_string = '{},{},{}'.format(olympian[0], olympian[1], olympian[2])
+    outfile.write(row_string)
+    outfile.write('\n')
+outfile.close()
 
 
 
 # Auto git saving #####################################################
 save = 1
-commitMsg = '1. Using with for Files' 
+commitMsg = '1. Writing data to a CSV File' 
 
 if save:
     print('\nAUTO GIT SAVING...')
