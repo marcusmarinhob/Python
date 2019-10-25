8c6b5bda7a2a95c86c2502280533fdc5bffdc0a5
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-10-25 13:36:45 -0300

    1. Returning a value from a function

--- Coursera/Python3Programming/02-PythonFunctionsFilesAndDictionaries/Functions.py
+++ Coursera/Python3Programming/02-PythonFunctionsFilesAndDictionaries/Functions.py
@@ -3,28 +3,16 @@ os.system('cls')
 
 print('\n# BEGIN OF THE SCRIPT #############################################')
 
-import turtle
-
-def drawSquare(t, sz):
-    """Make turtle t draw a square of with side sz."""
-
-    for i in range(4):
-        t.forward(sz)
-        t.left(90)
-
-
-wn = turtle.Screen()      # Set up the window and its attributes
-wn.bgcolor("lightgreen")
-
-alex = turtle.Turtle()    # create alex
-drawSquare(alex, 50)      # Call the function to draw the square passing the actual turtle and the actual side size
-
-wn.exitonclick()
+def square(x):
+    y = x * x
+    return y
+    
+print(square(2))
 
 
 # Auto git saving #####################################################
 save = 1
-commitMsg = '1. Functions - drawSquare' 
+commitMsg = '1. Returning a value from a function' 
 
 if save:
     print('\nAUTO GIT SAVING...')
