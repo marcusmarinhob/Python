ae483ecb6cd9468da1ac7df144d007bbfa5cac65
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2019-10-25 13:28:17 -0300

    1. Functions

---
+++ Coursera/Python3Programming/02-PythonFunctionsFilesAndDictionaries/Functions.py
@@ -0,0 +1,33 @@
+import os
+os.system('cls')
+
+print('\n# BEGIN OF THE SCRIPT #############################################')
+
+def hello():
+    """This function says hello and greets you"""
+    print("Hello")
+    print("Glad to meet you")
+
+hello()
+
+
+# Auto git saving #####################################################
+save = 1
+commitMsg = '1. Functions' 
+
+if save:
+    print('\nAUTO GIT SAVING...')
+    
+    fileName = os.path.basename(__file__)    
+
+    os.system('git add '+ fileName)
+    os.system('git commit -m '+'"'+commitMsg+'"')
+    os.system('git push')
+
+    print('\nFile Name: '+fileName)
+    print('\nCommit Message: '+commitMsg)
+    print('\nSAVED AND PUSHED!')
+
+
+print('\n# END OF THE SCRIPT ###############################################')
+#print('\n'*15)
